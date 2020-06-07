80fc76b6cfbc7e1885aef9fd27f2e509a1691b7d
Lorenzo Pafumi <lorenzo.pafumi@students.fhnw.ch>
2020-06-07 03:36:20 +0200

    added windpark controls in demo pane

--- src/main/java/cuie/loorenzo/windpark_dashboard/demo/DemoPane.java
+++ src/main/java/cuie/loorenzo/windpark_dashboard/demo/DemoPane.java
@@ -4,18 +4,23 @@ import cuie.loorenzo.windpark_dashboard.DashboardControl;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.util.converter.NumberStringConverter;
 
 public class DemoPane extends BorderPane {
 
     private final PresentationModel pm;
 
-    // declare the custom control
-    private DashboardControl cc;
+    private DashboardControl dashboard;
 
-    // all controls
     private Slider slider;
+    private TextField prod2015, prod2016, prod2017, prod2018, power;
+    private Label l1, l2, l3, l4, l5;
+    private HBox h1, h2, h3, h4, h5, h6, h7;
+    private VBox v1;
 
     public DemoPane(PresentationModel pm) {
         this.pm = pm;
@@ -26,22 +31,49 @@ public class DemoPane extends BorderPane {
 
     private void initializeControls() {
         setPadding(new Insets(10));
-        this.cc = new DashboardControl("Haldenstein", 4278, 4372, 4137, 4920, 6000, 3000, 8000);
+        this.prod2015 = new TextField();
+        this.prod2016 = new TextField();
+        this.prod2017 = new TextField();
+        this.prod2018 = new TextField();
+        this.power = new TextField();
+        this.l1 = new Label("Produktion 2015 (MWh)");
+        this.l2 = new Label("Produktion 2016 (MWh)");
+        this.l3 = new Label("Produktion 2017 (MWh)");
+        this.l4 = new Label("Produktion 2018 (MWh)");
+        this.l5 = new Label("Leistung (kW)");
+        this.h1 = new HBox(this.l1, this.prod2015);
+        this.h2 = new HBox(this.l2, this.prod2016);
+        this.h3 = new HBox(this.l3, this.prod2017);
+        this.h4 = new HBox(this.l4, this.prod2018);
+        this.h5 = new HBox(this.h1, this.h2);
+        this.h6 = new HBox(this.h3, this.h4);
+        this.h7 = new HBox(this.l5, this.power);
+        this.v1 = new VBox(this.h7, this.h5, this.h6);
+        this.dashboard = new DashboardControl("Haldenstein", 4278, 4372, 4137, 4920, 6000, 3000, 8000);
         this.slider = new Slider();
         this.slider.setShowTickLabels(true);
     }
 
     private void layoutControls() {
-        VBox controlPane = new VBox(new Label("Range Properties"), this.slider);
-        controlPane.setPadding(new Insets(0, 50, 0, 50));
-        controlPane.setSpacing(10);
-
-        setCenter(this.cc);
-        setRight(controlPane);
+        this.v1.setPadding(new Insets(50, 30, 50, 30));
+        this.v1.setSpacing(10);
+        this.h1.setSpacing(10);
+        this.h2.setSpacing(10);
+        this.h3.setSpacing(10);
+        this.h4.setSpacing(10);
+        this.h5.setSpacing(10);
+        this.h6.setSpacing(10);
+        this.h7.setSpacing(65);
+        setCenter(this.dashboard);
+        // setBottom(this.v1);
     }
 
     private void setupBindings() {
-        this.slider.valueProperty().bindBidirectional(this.pm.pmValueProperty());
+        this.power.textProperty().bindBidirectional(this.dashboard.getPowerDisplay().powerValueProperty(), new NumberStringConverter());
+        this.prod2015.textProperty().bindBidirectional(this.dashboard.getBar1().barValProperty(), new NumberStringConverter());
+        this.prod2016.textProperty().bindBidirectional(this.dashboard.getBar2().barValProperty(), new NumberStringConverter());
+        this.prod2017.textProperty().bindBidirectional(this.dashboard.getBar3().barValProperty(), new NumberStringConverter());
+        this.prod2018.textProperty().bindBidirectional(this.dashboard.getBar4().barValProperty(), new NumberStringConverter());
     }
 
 }
--- src/main/java/cuie/loorenzo/windpark_dashboard/demo/PresentationModel.java
+++ src/main/java/cuie/loorenzo/windpark_dashboard/demo/PresentationModel.java
@@ -4,18 +4,70 @@ import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 
 public class PresentationModel {
-    private final DoubleProperty pmValue = new SimpleDoubleProperty();
 
-    public double getPmValue() {
-        return this.pmValue.get();
+    private final DoubleProperty prod2015 = new SimpleDoubleProperty();
+    private final DoubleProperty prod2016 = new SimpleDoubleProperty();
+    private final DoubleProperty prod2017 = new SimpleDoubleProperty();
+    private final DoubleProperty prod2018 = new SimpleDoubleProperty();
+    private final DoubleProperty power = new SimpleDoubleProperty();
+
+    public double getProd2015() {
+        return this.prod2015.get();
     }
 
-    public DoubleProperty pmValueProperty() {
-        return this.pmValue;
+    public DoubleProperty prod2015Property() {
+        return this.prod2015;
     }
 
-    public void setPmValue(double pmValue) {
-        this.pmValue.set(pmValue);
+    public void setProd2015(double prod2015) {
+        this.prod2015.set(prod2015);
     }
 
+    public double getProd2016() {
+        return this.prod2016.get();
+    }
+
+    public DoubleProperty prod2016Property() {
+        return this.prod2016;
+    }
+
+    public void setProd2016(double prod2016) {
+        this.prod2016.set(prod2016);
+    }
+
+    public double getProd2017() {
+        return this.prod2017.get();
+    }
+
+    public DoubleProperty prod2017Property() {
+        return this.prod2017;
+    }
+
+    public void setProd2017(double prod2017) {
+        this.prod2017.set(prod2017);
+    }
+
+    public double getProd2018() {
+        return this.prod2018.get();
+    }
+
+    public DoubleProperty prod2018Property() {
+        return this.prod2018;
+    }
+
+    public void setProd2018(double prod2018) {
+        this.prod2018.set(prod2018);
+    }
+
+    public double getPower() {
+        return this.power.get();
+    }
+
+    public DoubleProperty powerProperty() {
+        return this.power;
+    }
+
+    public void setPower(double power) {
+        this.power.set(power);
+    }
 }
